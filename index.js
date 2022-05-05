12ea7dca6cdbb4cbd23191846100e34944206bf4
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-05 11:34:47 -0400

    commit tree 25200393f4dcb1af8c7dcd36e2322a7cefc67532

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x){
+if (x < 42){
+    return (42-x);
+}
+return (x-42);
+
+}
+function distanceFromHqInFeet(x){
+    let feet=0;
+    feet = distanceFromHqInBlocks(x)*264;
+    return feet;
+
+    }
+function distanceTravelledInFeet(x,y){
+    let d = 0;
+    if (x < y){
+        d=(y-x);
+    }else{
+        d=(x-y);   
+    }
+    return d*264;
+}
+function calculatesFarePrice(x, y) {
+    let d = distanceTravelledInFeet(x,y);
+    let a =0;
+    if(d<=400){
+        return 0;
+
+    }else if (d>400 && d<= 2000)
+    {
+        a= d-400;
+        return a*0.02;
+
+    }else if (d>2000 && d< 2500){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+
+
+  }
